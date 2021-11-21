fa9962d5830034e33274aa868d32eac88d067905
jacobshatzky <jacob.sipprellshatzky@hrsdc-rhdcc.gc.ca>
2021-11-21 11:38:46 -0500

    ddl

---
+++ DDL.sql
@@ -0,0 +1,107 @@
+create table bookstore
+	(ID					varchar(5) not null, 
+	 name				varchar(50) not null,
+	 email				varchar(50) not null, 
+	 primary key (ID)
+	);
+	
+create table book
+	(
+		ISBN			varchar(20) not null,
+		title			varchar(20) not null,
+		genre			varchar(20) not null,
+		pages			int NOT NULL,
+		price			decimal(5,2),    
+		primary key (ISBN)
+	);
+	
+create table author
+	(
+		ID					varchar(50) not null, 
+		name				varchar(50) not null,
+		address			varchar(50) not null,	
+		primary key (ID)
+	);
+	
+create table publisher
+	(ID			   		varchar(50) not null, 
+	 name				varchar(50) not null,
+	 address			varchar(50) not null,
+	 phone				varchar(20) not null, 
+	 email				varchar(50) not null,
+	 bank_account	    varchar(50) not null,		 
+	 primary key (ID)	
+	);
+	
+create table customer
+	(account_number		varchar(50) not null,
+	 name				varchar(50) not null,
+	 address			varchar(50) not null,
+	 phone				varchar(20) not null, 
+	 email				varchar(50) not null,
+	 primary key (account_number)		
+	);
+
+create table transaction
+	(transaction_id		varchar(50) not null, 
+	 shipping_number	varchar(50) not null,
+	 account_number		varchar(50) not null,
+	 
+	 primary key (transaction_id)		
+	);
+	
+create table reports
+	(transaction_id		varchar(50) not null, 
+	 shipping_number	varchar(50) not null,
+	 
+	 primary key (transaction_id)		
+	);
+	
+create table shopping_cart
+	(cart_id			varchar(50) not null, 
+	 account_number		varchar(50) not null,
+	 
+	 primary key (cart_id)		
+	);
+	
+create table holds	
+	(cart_id			varchar(50) not null, 
+	 ISBN				varchar(50) not null,
+	 pages				int NOT NULL,
+	 
+	 primary key (cart_id)		
+	);
+
+create table registers
+	(ID			   		varchar(50) not null, 
+	 account_number		varchar(50) not null,
+	 name				varchar(50) not null,
+	 address			varchar(50) not null,
+	 phone				varchar(20) not null, 
+	 email				varchar(50) not null,
+	 primary key (ID)		
+	);
+
+create table writtenBy
+	(ISBN			varchar(20) not null,
+	 ID				varchar(50) not null,
+	 name				varchar(50) not null,
+	 address			varchar(50) not null,
+	 
+	 primary key (ISBN)		
+	);
+
+create table publishedBy
+	(ISBN			varchar(20) not null,
+	 ID				varchar(50) not null,
+	 name				varchar(50) not null,
+	 address			varchar(50) not null,
+	 phone				varchar(20) not null, 
+	 email				varchar(50) not null,
+	 bank_account	    varchar(50) not null,
+	 
+	 primary key (ISBN)		
+	);
+
+
+	
