7433a4f7b1b198bd8ffa8ae0a8a2f758ec907812
jacobshatzky <66931648+jacobshatzky@users.noreply.github.com>
2021-12-10 19:17:29 -0500

    Update DDL.sql

--- sql/DDL.sql
+++ sql/DDL.sql
@@ -1,5 +1,5 @@
 create table bookstore
-	(bank_account    	decimal(5,2),
+	(bank_account    	decimal(5,2) 	not null
 	
 	);
 
@@ -44,67 +44,19 @@ create table customer
 	 email				varchar(50) not null,
 	 primary key (account_number)		
 	);
-
-create table transaction
-	(transaction_id		varchar(50) not null, 
-	 shipping_number	varchar(50) not null,
-	 account_number		varchar(50) not null,
-	 
-	 primary key (transaction_id)		
-	);
 	
 create table reports
 	(transaction_id		varchar(50) not null, 
-	 shipping_number	varchar(50) not null,
+	 total_price		decimal(5,2) not null,
+	 order_status		varchar(50) not null,
 	 
 	 primary key (transaction_id)		
 	);
 	
-create table shopping_cart
-	(cart_id			varchar(50) not null, 
-	 account_number		varchar(50) not null,
-	 
-	 primary key (cart_id)		
-	);
-	
-create table holds	
-	(cart_id			varchar(50) not null, 
-	 ISBN				varchar(50) not null,
-	 pages				int NOT NULL,
-	 
-	 primary key (cart_id)		
-	);
 
-create table registers
-	(ID			   		varchar(50) not null, 
-	 account_number		varchar(50) not null,
-	 name				varchar(50) not null,
-	 address			varchar(50) not null,
-	 phone				varchar(50) not null, 
-	 email				varchar(50) not null,
-	 primary key (ID)		
-	);
 
-create table writtenBy
-	(ISBN			varchar(50) not null,
-	 ID				varchar(50) not null,
-	 name				varchar(50) not null,
-	 address			varchar(50) not null,
-	 
-	 primary key (ISBN)		
-	);
 
-create table publishedBy
-	(ISBN			varchar(50) not null,
-	 ID				varchar(50) not null,
-	 name				varchar(50) not null,
-	 address			varchar(50) not null,
-	 phone				varchar(50) not null, 
-	 email				varchar(50) not null,
-	 bank_account	    varchar(50) not null,
-	 
-	 primary key (ISBN)		
-	);
+
 
 
 	
